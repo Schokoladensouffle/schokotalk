c856974406d11b20240c0eaa8a88160cec8930f4
Hendrik Oenings <hendrik.oenings@schokoladensouffle.eu>
2020-05-05 21:51:12 +0200

    Prohibit browser login form handling.
     * Prevent browser from using form methods for login.  Closes #1

--- login.js
+++ login.js
@@ -9,14 +9,16 @@ window.addEventListener('load', () => {
 		formext.value = formext.value.replace(/[^0-9]/gi, '');
 	});
 
-	formlogin.addEventListener('click', () => {
+	formlogin.addEventListener('click', (event) => {
+		event.preventDefault();
 		localStorage.setItem('exten', formext.value);
 		localStorage.setItem('username', formuser.value);
 		localStorage.setItem('password', formpass.value);
 		location.href = '/';
 	});
 
-	formanon.addEventListener('click', () => {
+	formanon.addEventListener('click', (event) => {
+		event.preventDefault();
 		localStorage.setItem('exten', exten);
 		localStorage.setItem('username', username);
 		localStorage.setItem('password', password);
